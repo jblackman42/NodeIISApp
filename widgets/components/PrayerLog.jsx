90bb87990c2d252b322144961693986971edb9a8
mbrandow21 <masonb@dauntlessit.com>
2024-02-27 17:04:17 -0700

    Have the basic data display showing now.

--- widgets/components/PrayerLog.jsx
+++ widgets/components/PrayerLog.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { FaTrash } from "react-icons/fa6";
+import { FaRegPenToSquare } from "react-icons/fa6";
+
+
 
 const Table = ({columns, data, rowKeyField}) => {
-  columns.pop()
+  // Create a new array excluding the last column, without mutating the original columns array
+  const updatedColumns = columns.slice(0, -1);
+  // console.log(updatedColumns)
+
   return(
     <div id='table-wrapper'>
       <table className='wpad-table'>
         <thead>
           <tr>
-            {columns.map(column => (
+            {updatedColumns.map(column => (
               <th key={column}>{column}</th>
             ))}
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {data.map(row => (
-            <tr id={row.rowKeyField}>
-              {columns.map(header => (
-                <td key={`${row.rowKeyField}-${header}`}>{String(row[header])}</td>
+            <tr key={row[rowKeyField]}>
+              {updatedColumns.map(header => (
+                <td key={`${row[rowKeyField]}-${header}`}>{row[header]}</td>
               ))}
+              <td className='icon'><FaRegPenToSquare /></td>
+              <td className='icon'><FaTrash /></td>
             </tr>
           ))}
         </tbody>
@@ -26,6 +37,7 @@ const Table = ({columns, data, rowKeyField}) => {
     </div>
   )
 }
+
 
 const PrayerLog = ({ requestURL }) => {
   const UserGUID = '153606b5-aaf6-4ad2-84ad-643909e664fb';
@@ -48,11 +60,22 @@ const PrayerLog = ({ requestURL }) => {
       throw error;
     });
   };
+  const formatDateString = (isoString) => {
+    const date = new Date(isoString);
+    const dateString = date.toLocaleDateString('en-us', {year: 'numeric', month: 'short', day: 'numeric'});
+    const timeString = date.toLocaleTimeString('en-us', {hour: 'numeric', minute: 'numeric'});
+    return `${dateString} - ${timeString}`
+  }
 
   useEffect(() => {
     getPrayerInformation(UserGUID)
       .then(prayerinfo => {
-        setDataArray(prayerinfo)
+        const updatedData = prayerinfo.map(item => {
+          const formattedDate = formatDateString(item.Start_Date);
+          return { ...item, Start_Date: formattedDate };
+        });
+        console.log(updatedData)
+        setDataArray(updatedData)
         setHeaders(prayerinfo.length > 0 ? Object.keys(prayerinfo[0]) : [])
       })
       .catch(err => {
